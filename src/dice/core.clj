daffc9c6de97990a864e9e61f8d18cf0b3565d6e
Ernie <ernie_hs@hotmail.com>
2017-03-18 16:46:20 +0100

    initial revision

--- src/dice/core.clj
+++ src/dice/core.clj
@@ -1,5 +1,16 @@
 (ns dice.core
   (:gen-class))
 
+(defn rolld
+  "rolls a d-sided die"
+  ([] nil)
+  ([d] (+ (rand-int d) (if (= d 0) 0 1))))
 
+(defrecord Die [sides])
+
+(defn roll
+  "rolls a single die, or dice"
+  ([] nil)
+  ([die] (rolld (:sides die)))
+  ([die & more] (map roll (conj more die))))
 
