5def0667d0ff2a2c15e453048f96a6092df363ac
Ernie <ernie_hs@hotmail.com>
2017-03-19 14:39:57 +0100

    some tests

--- src/dice/core.clj
+++ src/dice/core.clj
@@ -2,20 +2,26 @@
   (:gen-class))
 
 (defn rolld
-  "rolls a d-sided die"
+  "rolls a d-sided die, no arg or nil results in nil"
   ([] nil)
-  ([d] (+ (rand-int d) (if (= d 0) 0 1))))
+  ([d] (if (nil? d) nil (+ (rand-int d) (if (= d 0) 0 1)))))
 
 (defrecord Die [sides])
 
 (defn make-die
-  "makes an n sided die"
-  ([] nil)
-  ([n] (Die. n))) 
+  "makes an n sided die, n > 1 or a d6 is created!"
+  ([] (Die. 6))
+  ([n] (if (nil? n) (Die. 6) (if (> n 1) (Die. n) (Die. 6))))) 
 
+;
+; I was thinking of using a protocol...
+;
 (defn roll
-  "rolls a single die, or dice"
+  "rolls a die, or dice"
   ([] nil)
   ([die] (rolld (:sides die)))
   ([die & more] (map roll (conj more die))))
 
+(def discard-lowest
+  [] nil
+  [dice] ())
--- test/dice/core_test.clj
+++ test/dice/core_test.clj
@@ -2,9 +2,33 @@
   (:use clojure.test)
   (:require [dice.core :as core]))
 
+(def repeat-count 30)
+
+(deftest rolld-n
+  (is (nil? (core/rolld)))
+  (is (nil? (core/rolld nil)))
+  (is (= (core/rolld 0) 0))
+  (is (= (core/rolld 1) 1))
+  (repeatedly repeat-count (is (<= 1 (core/rolld 10) 10))))
+
 (deftest create-die
-  (is (= 8 (:sides (core/make-die 8)))))
+  (is (= 6 (:sides (core/make-die))))
+  (is (= 6 (:sides (core/make-die nil))))
+  (is (= 6 (:sides (core/make-die 1))))
+  (is (= 6 (:sides (core/make-die -1))))
+  (is (= 8 (:sides (core/make-die 8))))
+  (is (= 12 (:sides (core/make-die 12)))))
 
 (deftest roll-die
   (let [d8 (core/make-die 8)] 
-    (is (<= 1 (core/roll d8) 8))))
+    (repeatedly repeat-count (is (<= 1 (core/roll d8) 8)))))
+
+(deftest roll-dice
+  (let [d4 (core/make-die 4)
+        d6 (core/make-die 6)
+        d8 (core/make-die 8) 
+        d12 (core/make-die 12)]
+    (is (nil? (core/roll)))
+    (repeatedly repeat-count (is (= (count (core/roll d4 d12)) 2)))
+    (repeatedly repeat-count (is (= (count (core/roll d4 d6 d8 d12)) 4)))
+    (repeatedly repeat-count (is (<= 4 (reduce + (core/roll d4 d6 d8 d12)) 30)))))
